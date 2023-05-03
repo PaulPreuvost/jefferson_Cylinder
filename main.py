5916102b4d2de654e4be974aec1c5ffa6148ccc5
PaulPreuvost <118807111+PaulPreuvost@users.noreply.github.com>
2023-05-03 11:18:53 +0200

    now

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ import random
 
 class main:
     def __init__(self):
-        #Initialise les valeurs 
+        # Initialise les valeurs
         self.__alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         self.__length_Alphabet = len(self.__alphabet)
         self.__number_Of_Disk = 26
         self.__length_Of_Disk = self.__length_Alphabet
-        self.Disk = [self.__alphabet] * self.__number_Of_Disk
+        self.Disk = []
+        # Ajouter chaque ligne dans un ordre différent
+        for i in range(self.__number_Of_Disk):
+            #Ajoute l'alphabet dans le bon ordre à la ligne 6, l'ajoutant dans le self.Disk
+            if i == 5:  # sixième ligne
+                self.Disk.append(self.__alphabet.copy())  # copie de l'alphabet
+            #Ajoute l'alphabet dans un ordre différent à chaque ligne en ajoutant dans le self.Disk
+            else:
+                temp = self.__alphabet.copy()
+                random.shuffle(temp)
+                self.Disk.append(temp)
         self.row = 0
         self.column = 0
 
     def view(self):
-        #Affichage console du cylindre 
-        system("cls")
+        # Affichage console du cylindre
+        system("clear")
         print("Start")
         for self.row in range(self.__number_Of_Disk):
             for self.column in range(self.__length_Alphabet):
@@ -25,13 +35,13 @@ class main:
         print("Affichage Console..... OK !")
 
     def read_TXT (self):
-        #Lit le fichier input.txt et efface son contenu
+        # Lit le fichier input.txt et efface son contenu
         with open('TXT/input.txt', 'w') as input:
             input.write('')
         print("Lecture Fichier..... OK !")
 
     def write_TXT (self):
-        #Écrit la grille dans le fichier input.txt
+        # Écrit la grille dans le fichier input.txt
         with open('TXT/input.txt', 'w') as output:
             for self.row in range(self.__number_Of_Disk):
                 for self.column in range(self.__length_Alphabet):
@@ -39,12 +49,7 @@ class main:
                 output.write('\n')
         print("Écriture Fichier..... OK !")
 
-Launch = main();
-Launch.view();
-Launch.read_TXT();
-Launch.write_TXT();
-
-
-#class view : 
-#class read_TXT :    
-#class write_TXT :
+Launch = main()
+Launch.view()
+Launch.read_TXT()
+Launch.write_TXT()
